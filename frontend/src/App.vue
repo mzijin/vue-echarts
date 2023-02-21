9a069afa82f9fadbb411443f55dc81f18e1bb15a
Williams <1602910132@qq.com>
2023-02-21 23:15:34 +0800

    Changes

---
+++ frontend/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+    <div id="app">
+        <EchartMap :mapData="mapData" style="width: 100%;height: 100vh" :mapOption="{
+            'title': '地图',
+            'titleColor': 'pink'
+        }"></EchartMap>
+    </div>
+</template>
+<script>
+    import EchartMap from './components/EchartMap'
+    export default {
+        components: {
+            EchartMap
+        },
+        data() {
+            return {
+                    mapData: []
+            }
+        },
+        methods: {
+    initData () {
+      this.$ajax.get('http://127.0.0.1:5000/data', {}).then(res => {
+        this.mapData = res.data.flight;
+      })
+    }
+  },
+  created () {
+    this.initData()
+  }
+    }
+
+</script>
+<style>
+    * {
+        margin: 0;
+        padding: 0;
+    }
+
+</style>
